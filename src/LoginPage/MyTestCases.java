efcf3aec9c647b598ff438abde9507cf70c9b6db
Dassoki <abdulrahmandassoqi@gmail.com>
2025-07-05 21:47:01 +0300

    update

--- src/LoginPage/MyTestCases.java
+++ src/LoginPage/MyTestCases.java
@@ -1,5 +1,7 @@
 package LoginPage;
 
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,6 +14,8 @@ public class MyTestCases {
 	WebDriver driver = new ChromeDriver(); 
 	String theURL = "https://automationteststore.com/";
 	String SignupPage = "https://automationteststore.com/index.php?rt=account/create"; 
+	Random rand = new Random();
+	
 	
 	@BeforeTest
 	public void mySetup() {
@@ -26,11 +30,60 @@ public class MyTestCases {
 	public void Signup() {
 		
 		driver.navigate().to(SignupPage);
+		//element
+		WebElement FirstNameInput = driver.findElement(By.xpath("//*[@id=\"AccountFrm_firstname\"]"));
+		WebElement LastNameInput = driver.findElement(By.id("AccountFrm_lastname"));
+		WebElement EmailInput = driver.findElement(By.id("AccountFrm_email"));
+		WebElement TelephoneInput = driver.findElement(By.id("AccountFrm_telephone"));
+		WebElement FaxInput = driver.findElement(By.id("AccountFrm_fax"));
+		WebElement CompanyInput = driver.findElement(By.id("AccountFrm_company"));
+		WebElement Address1Input = driver.findElement(By.id("AccountFrm_address_1"));
+		WebElement Address2Input = driver.findElement(By.id("AccountFrm_address_2"));
+		WebElement CityInput = driver.findElement(By.id("AccountFrm_city"));
+		WebElement PostalCodeInput = driver.findElement(By.id("AccountFrm_postcode"));
+		WebElement LoginInput = driver.findElement(By.id("AccountFrm_loginname"));
+		WebElement PassowrdInput = driver.findElement(By.id("AccountFrm_password"));
+		WebElement Passowrd2Input = driver.findElement(By.id("AccountFrm_confirm"));
 		
-		WebElement firstName = driver.findElement(By.xpath("//*[@id=\"AccountFrm_firstname\"]"));
 		
-		firstName.sendKeys("Abdulrahman");
-		//test
+		//data
+		String [] FirstName = {"Abdulrahman" , "Abdllah" , "Ali" , "Samer"};
+		int randomIndexForFirstNames = rand.nextInt(FirstName.length);
+		
+		String RandomFirstNames = FirstName[randomIndexForFirstNames];
+		
+		String [] LastName = {"AlSammer" , "AlAbdullah" , "Bassam" , "Nizar"};
+		int randomIndexLastNames = rand.nextInt(LastName.length);
+		
+		String RandomLastNames = LastName[randomIndexLastNames];
+		
+		
+		int randomnumberfortheemail = rand.nextInt(7000);
+		String Email = RandomFirstNames+RandomLastNames+randomnumberfortheemail+"@gmail.com";
+		String Telephone = "07777777";
+		String Fax = "555214";
+		String Company = "iTchjo";
+		String Address1= "Amman";
+		String Address2= "Abdoun";
+		String City = "AmmanGov";
+		String PostalCode = "12254";
+		String Login = RandomFirstNames+RandomLastNames;
+		String Password = "Test@12345";
+		
+		//action
+		FirstNameInput.sendKeys(RandomFirstNames);
+		LastNameInput.sendKeys(RandomLastNames);
+		EmailInput.sendKeys(Email);
+		TelephoneInput.sendKeys(Telephone);
+		FaxInput.sendKeys(Fax);
+		CompanyInput.sendKeys(Company);
+		Address1Input.sendKeys(Address1);
+		Address2Input.sendKeys(Address2);
+		CityInput.sendKeys(City);
+		PostalCodeInput.sendKeys(PostalCode);
+		LoginInput.sendKeys(Login);
+		PassowrdInput.sendKeys(Password);
+		Passowrd2Input.sendKeys(Password);
 	}
 
 	
